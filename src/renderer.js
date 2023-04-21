f54500909aa03585ddd455a1471558f058240472
zhounie <iszhounie@aliyun.com>
2023-04-21 17:50:20 +0800

    init

---
+++ src/renderer.js
@@ -0,0 +1,177 @@
+
+const { desktopCapturer, remote } = require('electron')
+
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname, './public/pen.png')
+
+const canvas = document.getElementById('canvas')
+const plan = document.getElementById('draw-plan')
+canvas.width = plan.clientWidth - 20
+canvas.height = plan.clientHeight - 20
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = '#fff'
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+let penColor = '#000000'
+ctx.strokeStyle = penColor
+
+ctx.lineJoin="round";
+ctx.lineCap="round";
+ctx.lineWidth = 10;
+// fs.readFile()
+console.log(filePath);
+const img = new Image();
+img.src = filePath
+img.onload = function() {
+  // 将自定义鼠标图像绑定到 Canvas 元素
+  // canvas.style.cursor = ``;
+};
+
+
+let drawing = false;
+let lastX = 0;
+let lastY = 0;
+
+const createInput = (e) => {
+  let inputDom = plan.getElementsByTagName('input')
+  if (inputDom.length) {
+    for(var i = 0; i < inputDom.length; i++) {
+      inputDom[i].blur()
+    }
+  }
+  const input = document.createElement('input')
+  input.id = 'text-input'
+  input.type = 'text'
+  input.style.left = `${e.offsetX}px`
+  input.style.top = `${e.offsetY}px`
+  input.autofocus = true
+  input.onkeydown = (e) => {
+    const code = e.code
+    if (code === 'Enter') {
+      input.blur()
+    }
+  }
+  input.onblur = () => {
+    const text = input.value
+    let inputDom = plan.getElementsByTagName('input')
+    if (inputDom.length) {
+      for(var i = 0; i < inputDom.length; i++) {
+        plan.removeChild(inputDom[i])
+      }
+    }
+    // plan.removeChild(input)
+    if (text.length > 0) {
+      ctx.font = '24px sans-serif';
+      ctx.fillStyle = getColor()
+      ctx.fillText(text, e.offsetX, e.offsetY);
+    }
+  }
+  plan.appendChild(input)
+}
+
+function startDrawing(e) {
+  drawing = true;
+  if (checked === 1) {
+    createInput(e)
+  } else {
+    if (checked === 0) {
+      // ctx.globalCompositeOperation = 'destination-out'
+    }
+    // 绘制普通线条
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    ctx.lineWidth = getThickness(); // 设置当前笔触粗细
+    ctx.strokeStyle = checked === 0 ? '#fff' : getColor()
+  }
+}
+
+function draw(e) {
+  if (!drawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+function stopDrawing() {
+  ctx.globalCompositeOperation = 'source-over'; // 恢复混合模式为 source-over
+  drawing = false;
+}
+
+const colorPicker = document.getElementById('color-picker');
+
+function getColor() {
+  return colorPicker.value;
+}
+
+const thicknessPicker = document.getElementById('thickness-picker');
+
+function getThickness() {
+  return parseInt(thicknessPicker.value);
+}
+
+const textInput = document.getElementById('text-input');
+
+function getText() {
+  return textInput.value;
+}
+const xp = document.getElementById('xp')
+const wz = document.getElementById('wz')
+const hb = document.getElementById('hb')
+let checked = 3
+function onSetXP () {
+  hb.className = ''
+  wz.className = ''
+  checked = 0
+  xp.classList.add('active')
+}
+function onSetWZ () {
+  hb.className = ''
+  xp.className = ''
+  checked = 1
+  wz.classList.add('active')
+}
+function onSetHB () {
+  xp.className = ''
+  wz.className = ''
+  checked = 2
+  hb.classList.add('active')
+}
+
+const onSave = () => {
+  const dataURL = canvas.toDataURL(); // 获取画布数据 URL
+  const link = document.createElement('a'); // 创建一个链接元素
+  link.download = 'my-canvas.png'; // 设置下载文件名
+  link.href = dataURL; // 将数据 URL 赋值给链接 href 属性
+  document.body.appendChild(link); // 将链接添加到文档中
+  link.click(); // 模拟点击链接以触发下载
+  document.body.removeChild(link); // 下载完成后将链接从文档中移除
+}
+
+
+const importImage = document.getElementById('import-image');
+
+importImage.addEventListener('change', function(e) {
+  const file = e.target.files[0]; // 获取选择的文件
+  const reader = new FileReader(); // 创建文件读取器
+
+  reader.onload = function(event) { // 文件读取成功后执行
+    const image = new Image();
+    image.src = event.target.result; // 获取文件数据 URL
+    image.onload = function() { // 图片加载完成后执行
+      ctx.drawImage(image, 0, 0); // 将图片绘制到画布上
+    };
+  };
+
+  reader.readAsDataURL(file); // 读取文件数据
+});
+const eraserToggle = document.getElementById('eraser-toggle');
+const textToggle = document.getElementById('text-toggle');
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseout', stopDrawing);
